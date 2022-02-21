f76c212f02af90d8fc8ee8128f80c1a9a417838d
Firshta Shefa <fshefa@bellevuewa.gov>
2022-02-21 13:53:59 -0800

    added font responsive

--- src/Componenets/HeroArea.js
+++ src/Componenets/HeroArea.js
@@ -1,26 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
 const HeroArea = () => {
   return (
-    <div className="jumbotron jumbotron-fluid p-0">
-      <Container fluid={true}>
-        <Row
-          className="justify-content-center py-5
+    <StyledWrapper>
+      <div className="jumbotron jumbotron-fluid p-0">
+        <Container fluid={true}>
+          <Row
+            className="justify-content-center py-5
         "
-        >
-          <Col sm={12} md={8} lg={4}>
-            <h2 className="display-1 font-weight-bolder">
-              I am a Photographer
-            </h2>
-            <h3 className="display-4 font-weight-light">
-              Contact me for more information
-            </h3>
-            <Button>Hire me</Button>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+          >
+            <Col sm={8} md={6} lg={4}>
+              <h2 className="display-1 font-weight-bolder">
+                I am a Photographer
+              </h2>
+              <h3 className="display-4 font-weight-light">
+                Contact me for more information
+              </h3>
+              <Button>Hire me</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </StyledWrapper>
   );
 };
+
 export default HeroArea;
+const StyledWrapper = styled.div`
+  h2,
+  h3 {
+    font-size: 8vw;
+  }
+`;
